b101b74638393702d3f57fd02f6410386892a0ae
Mitch Durso <starystars67@gmail.com>
2019-04-24 12:30:37 +0100

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,13 @@
+local function OnPlayerConnecting(name, setKickReason, deferrals)
+    deferrals.defer()
+    local cname = string.gsub(name, "%s+", "")
+    deferrals.update(string.format("Hello %s. Your name is being checked.", name))
+
+    if string.find(str, "<script") then
+        deferrals.done("Your username seems to be fishy...")
+    else
+        deferrals.done()
+    end
+end
+
+AddEventHandler("playerConnecting", OnPlayerConnecting)
